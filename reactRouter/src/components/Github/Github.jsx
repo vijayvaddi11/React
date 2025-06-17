e005a53401dbe3ab8d1ce952377e9b137ca746fc
vijay vaddi <vijayjay6688@gmail.com>
2025-06-17 20:21:24 +0530

    add portfolio demo to github

---
+++ reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react'
+
+function Github() {
+  const [data,setData]=useState([])
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/vijayvaddi11')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data) 
+        setData(data)
+      })
+    
+  },[])
+  return (
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>
+      github followers:{data.followers}
+      <img src={data.avatar_url} alt="Git picture" width={200} />
+      <p>{data.bio}</p>
+    </div>
+  )
+}
+
+export default Github
---
+++ reactRouter/src/main.jsx
@@ -0,0 +1,56 @@
+import { Route } from 'react-router-dom';
+import { Children, StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom';
+import Layout from './Layout';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import User from './components/User/User';
+import Github from './components/Github/Github';
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: '',
+//         element: <Home/>
+//       },
+//       {
+//         path: 'about',
+//         element:<About />
+
+//       },
+//       {
+//         path: 'contact',
+//         element:<Contact/>
+//       }
+//     ]
+//   }
+// ])
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+      <Route path='user/:id' element={<User />} />
+      <Route path='github' element={<Github/>}/>
+    </Route>
+  )
+);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
